b6c23709eec773cf00a589b1bcf488d72ed7628f
usuario <usuario@sim06.ujaen.es>
2018-02-08 11:47:42 +0100

    servlet1 modificado

--- pr0/isst2018pr0/src/paquete1/servlet1.java
+++ pr0/isst2018pr0/src/paquete1/servlet1.java
@@ -28,6 +28,8 @@ public class servlet1 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		//comentario para crear version diferente
 	}
 
 	/**
