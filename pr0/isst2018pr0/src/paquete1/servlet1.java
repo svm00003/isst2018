7d29a8b114d58e04810317fc8cdc57a5aa1fcfc6
x2p1 <x2p1@10.82.9.193>
2018-02-22 12:18:06 +0100

    index movido

--- pr0/isst2018pr0/src/paquete1/servlet1.java
+++ pr0/isst2018pr0/src/paquete1/servlet1.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class servlet1
  */
-@WebServlet("/servlet1")
+@WebServlet("/servlet1");
 public class servlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
