0dd191d60fd8560a606bc9834e93c069cdf3c499
x2p1 <x2p1@10.82.9.193>
2018-02-22 12:23:28 +0100

    servlet con codigo inicial de cookies

--- pr0/isst2018pr0/src/paquete1/servlet1.java
+++ pr0/isst2018pr0/src/paquete1/servlet1.java
@@ -45,3 +45,27 @@ public class servlet1 extends HttpServlet {
 	}
 
 }
+
+/*
+@WebServlet("/HoraServlet")
+public class HoraServlet extends HttpServlet {
+protected void doGet(HttpServletRequest request, HttpServletResponse response)
+throws ServletException, IOException {
+HttpSession sesion = request.getSession(true);
+response.setContentType("text/html");
+PrintWriter html = response.getWriter();
+html.print("<strong>");
+Date date = (Date)sesion.getAttribute("date");
+if(date != null) {
+html.print("Último acceso de la sesión: " + date + "<br>");
+}
+else {
+html.print("Este es el primer acceso de la sesión <br>");
+}
+date = new Date();
+sesion.setAttribute("date", date);
+html.print("Fecha actual: " + date);
+html.print("</strong>");
+}
+}
+*/
